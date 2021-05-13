2cab50f7db1f70284eff51c2229edc19837aefde
biyanoscar <biyanoscar@gmail.com>
2021-05-13 20:12:25 +0700

    Submission 1: custom menu button hamburger

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
+import '../styles/menu.css';
 import data from "../DATA.json";
 
 console.log('Hello Coders! :)');
@@ -11,10 +12,18 @@ const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
 
-menu.addEventListener('click', function (event) {
-    drawer.classList.toggle('open');
+const topMenu = document.querySelector('.top-menu');
+const menuBtn = document.querySelector('#menu-button');
+menuBtn.addEventListener('click', function (event) {
+    topMenu.classList.toggle('active');
+    menuBtn.classList.toggle('menu-closed');
     event.stopPropagation();
 });
+
+// menu.addEventListener('click', function (event) {
+//     drawer.classList.toggle('open');
+//     event.stopPropagation();
+// });
 
 hero.addEventListener('click', function () {
     drawer.classList.remove('open');
@@ -36,15 +45,21 @@ data.restaurants.forEach(addRestaurantCard);
 function addRestaurantCard(item, index) {
     document.getElementById("restaurants").innerHTML +=
     `<article class="restaurant-item">
-        <img class="restaurant-item__thumbnail"
-            src="${item['pictureId']}"
-            alt="${item['name']}">
+        <div class="list-card-image">
+            <div class="rating position-absolute">
+                <span class="badge badge-rating">Rating: ${item['rating']} </span>
+            </div>
+            <div class="member-plan position-absolute">
+                <span class="badge badge-city">${item['city']}</span>
+            </div>
+            <img class="restaurant-item__thumbnail"
+                src="${item['pictureId']}"
+                alt="${item['name']}">
+        </div>
         <div class="restaurant-item__content">
-            <p class="restaurant-item__date">Rating: ${item['rating']}
-            </p>
             <h1 class="restaurant-item__title">${item['name']}</h1>
             <p class="restaurant-item__description text">${item['description']}</p>
-            <p class="restaurant-item__footer">${item['city']}</p>
+            
         </div>
     </article>`
     //   document.getElementById("demo").innerHTML += index + ":" + item + "<br>";
