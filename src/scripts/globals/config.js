c8131217eb262a312cbcf720df3de25c5867a912
biyanoscar <biyanoscar@gmail.com>
2021-06-03 18:36:47 +0700

    feat: PWA Cache

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,6 +3,8 @@ const CONFIG = {
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   BASE_IMAGE_MEDIUM_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   DEFAULT_LANGUAGE: 'en-us',
+  // CACHE_NAME: 'PergiMakan-V1',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
