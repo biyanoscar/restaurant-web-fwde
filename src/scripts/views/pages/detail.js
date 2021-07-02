27adc75fa84b6f0e5af0d84a0dadda017475c4ae
biyanoscar <biyanoscar@gmail.com>
2021-07-02 20:32:19 +0700

    TDD: Integration Test - arsitektur dirubah jadi Model-View-Presenter - modul helper testFactories untuk code inisialisasi like button - refaktor code test memakai testFactories - rename function template tombol, dirubah jd like & unlike button

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,7 +1,7 @@
 import UrlParser from '../../routes/url-parser';
 import RestaurantDicodingSource from '../../data/restaurant-dicoding-source';
 import { createRestaurantDetailTemplate } from '../templates/template-creator';
-import LikeButtonInitiator from '../../utils/like-button-initiator';
+import LikeButtonPresenter from '../../utils/like-button-presenter';
 
 const Detail = {
   async render() {
@@ -23,7 +23,7 @@ const Detail = {
 
     restaurantsContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
 
-    LikeButtonInitiator.init({
+    LikeButtonPresenter.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       restaurant: {
         id: restaurant.id,
