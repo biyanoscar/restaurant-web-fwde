5939104907abe3af6a27536908f2eb34a2ecb042
biyanoscar <biyanoscar@gmail.com>
2021-07-10 08:52:49 +0700

    Compress image sharpjs

--- sharp.js
+++ sharp.js
@@ -14,6 +14,7 @@ fs.readdirSync(target)
     // mengubah ukuran gambar dengan lebar 800px, dengan prefix -large.jpg
     sharp(`${target}/${image}`)
       .resize(800)
+      .jpeg({ quality: 30 })
       .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
         .slice(0, -1)
         .join('.')}-large.jpg`));
@@ -21,6 +22,7 @@ fs.readdirSync(target)
     // mengubah ukuran gambar dengan lebar 480px, dengan prefix -small.jpg
     sharp(`${target}/${image}`)
       .resize(480)
+      .jpeg({ quality: 30 })
       .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
         .slice(0, -1)
         .join('.')}-small.jpg`));
